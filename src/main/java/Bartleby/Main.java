b0254c04812431cdf11ccac5adb6ed9e383de6b4
juan buitrago <buitrago_je@javeriana.edu.co>
2022-10-24 18:20:09 -0500

    se realiza commit con el JDBC respectivo para la base de datos y la dependencia de Maven para la conexion con MySQL

--- src/main/java/Bartleby/Main.java
+++ src/main/java/Bartleby/Main.java
@@ -1,7 +1,28 @@
 package Bartleby;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "jdbc:mysql://localhost:3306/bartleby";
+        String user ="root";
+        String password ="PHSGOMNYDLHSLWTW";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url,user,password);
+            System.out.println("successfully connected to the database "+url);
+            String query ="Insert into bartleby.carta(idcarta,direccionEntrega,tipoServicio,owner) values (101,'cra 123 #45-67','0',102)";
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate(query);
+
+        } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
